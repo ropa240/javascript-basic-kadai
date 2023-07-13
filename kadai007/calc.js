57485d0278a1681e16b8427a0ba7cd98998ab556
ropa240 <135818760+ropa240@users.noreply.github.com>
2023-07-13 17:16:48 +0900

    Add files via upload

---
+++ kadai007/calc.js
@@ -0,0 +1,20 @@
+ let num = Math.floor(Math.random() * 15);
+
+ console.log(num);
+
+
+ if (num === 3) {
+  console.log('3の倍数です');
+}
+
+else if (num % 3=== 0) {
+  console.log('3の倍数です');
+}
+
+if (num === 5) {
+  console.log('5の倍数です');
+}
+
+else if (num % 5=== 0) {
+  console.log('5の倍数です');
+}
